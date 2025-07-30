843b8377be6720c8794d0770c21b5d49f3bb69da
prussematheus <matheus_f_prusse@estudante.sesisenai.org.br>
2025-07-30 12:36:23 -0300

    Update  -Dropdown para Produtos

--- script/script.js
+++ script/script.js
@@ -2,3 +2,19 @@ const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav");
 
 hamburger.addEventListener("click", () => nav.classList.toggle("active"));
+
+  document.addEventListener('DOMContentLoaded', function () {
+        const dropbtn = document.querySelector('.dropbtn');
+        const dropdownContent = document.querySelector('.dropdown-content');
+
+        dropbtn.addEventListener('click', function (e) {
+          e.preventDefault();
+          dropdownContent.classList.toggle('show');
+        });
+
+        document.addEventListener('click', function (e) {
+          if (!e.target.closest('.dropdown')) {
+            dropdownContent.classList.remove('show');
+          }
+        });
+      });
